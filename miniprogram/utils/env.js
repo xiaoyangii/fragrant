13523dbd2da98732f075fdf12f130cc3032cda07
xiaoyangii <1210693637@qq.com>
2024-03-17 15:49:00 +0800

    feat: add env config :sparkles:

---
+++ miniprogram/utils/env.js
@@ -0,0 +1,34 @@
+// 配置环境变量
+
+// 获取账号信息
+const { miniProgram } = wx.getAccountInfoSync()
+
+// 获取版本
+const { envVersion } = miniProgram
+
+let env = {
+  baseURL: 'https://gmall-prod.atguigu.cn/mall-api'
+}
+
+switch (envVersion) {
+  // 开发版
+  case 'develop':
+    env.baseURL = 'https://gmall-prod.atguigu.cn/mall-api'
+    break
+
+  // 体验版
+  case 'trial':
+    env.baseURL = 'https://gmall-prod.atguigu.cn/mall-api'
+    break
+
+  // 正式版
+  case 'release':
+    env.baseURL = 'https://gmall-prod.atguigu.cn/mall-api'
+    break
+
+  default:
+    env.baseURL = 'https://gmall-prod.atguigu.cn/mall-api'
+    break
+}
+
+export { env }
