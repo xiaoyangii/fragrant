59dbc53650c19d6e2a1baf6880735eb9c0f4d122
xiaoyangii <1210693637@qq.com>
2024-03-17 16:44:58 +0800

    feat: add category module :sparkles:

--- miniprogram/pages/category/category.js
+++ miniprogram/pages/category/category.js
@@ -1,1 +1,31 @@
-Page({})
+import { reqCategoryData } from '../../api/category'
+
+Page({
+  data: {
+    categoryList: [], // 分类数据列表
+    activeIndex: 0 // actived一级分类的索引，默认 0
+  },
+
+  onLoad(options) {
+    // 获取页面中使用的
+    this.getCategoryData()
+  },
+
+  // 实现一级分类的切换效果
+  updateActive(event) {
+    const { index } = event.currentTarget.dataset
+
+    this.setData({
+      activeIndex: index
+    })
+  },
+
+  // 获取页面初始化数据
+  async getCategoryData() {
+    const res = await reqCategoryData()
+
+    this.setData({
+      categoryList: res.data
+    })
+  }
+})
