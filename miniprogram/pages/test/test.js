0a223f221581c7516514d45fbfc95627e6eda96a
xiaoyangii <1210693637@qq.com>
2024-03-20 14:18:00 +0800

    feat: add test :white_check_mark:

--- miniprogram/pages/test/test.js
+++ miniprogram/pages/test/test.js
@@ -1,10 +1,12 @@
 // pages/test/test.js
 import instance from '../../utils/http'
 import { reqSwiperData } from '../../api/index'
+import Schema from 'async-validator'
 
 Page({
   data: {
-    avatarUrl: '../../assets/images/avatar.png'
+    avatarUrl: '../../assets/images/avatar.png',
+    name: ''
   },
 
   // 获取首页数据
@@ -90,5 +92,114 @@ Page({
       isLoading: false
     })
     console.log(res)
+  },
+
+  // 拒绝授权后的处理方案
+  async onLocation1() {
+    try {
+      // 获取用户地理位置信息
+      // 在用户拒绝授权以后，如果再次调用 getLocation，不会在弹出授权弹窗
+      const res = await wx.getLocation()
+      console.log(res)
+    } catch (error) {
+      wx.toast({ title: '您拒绝授权获取地理位置' })
+    }
+  },
+
+  // 拒绝授权后的处理方案-获取授权状态
+  async onLocation2() {
+    // 调用 wx.getSetting 获取用户所有的授权信息，查询到用户是否授权了位置信息
+    // authSetting 只包含了小程序向用户请求的所有的权限，同时包含了授权的结果(true、false)
+    const { authSetting } = await wx.getSetting()
+
+    // scope.userLocation 用户是否授权获取了地理位置信息
+    // 如果小程序没有向用户发起过授权请求，authSetting 中没有 scope.userLocation 属性
+    // 如果用户点击了允许授权，返回值就是 true
+    // 如果用户点击了拒绝授权，返回值就是 false
+    console.log(authSetting['scope.userLocation'])
+  },
+
+  // 拒绝授权后的处理方案-整体的逻辑
+  async onLocation() {
+    // authSetting 获取小程序已经向用户申请的权限，并且会返回授权的结果
+    const { authSetting } = await wx.getSetting()
+    // scope.userLocation 用户是否授权小程序获取位置信息
+    console.log(authSetting['scope.userLocation'])
+
+    // 判断用户是否拒绝了授权
+    if (authSetting['scope.userLocation'] === false) {
+      // 用户之前拒绝授权获取位置信息，用户再次发起了授权
+      // 这时候需要使用一个弹框询问用户是否进行授权
+      const modalRes = await wx.modal({
+        title: '授权提示',
+        content: '需要获取地理位置信息，请确认授权',
+        success: async function (res) {
+          if (res.confirm) {
+            // 如果用户点击了确定，说明用户同意授权，需要打开微信客户端小程序授权页面
+            const { authSetting } = await wx.openSetting()
+            // 如果用户没有更新授权信息，需要给用户提示授权失败
+            if (!authSetting['scope.userLocation']) return wx.toast({ title: '授权失败' })
+          }
+        }
+      })
+
+      // 如果用户点击了取消，说明用户拒绝了授权，需要给用户进行提示
+      if (!modalRes) return wx.toast({ title: '您拒绝了授权' })
+
+      //如果用户更新了授权信息，说明用户同意授权获取位置信息
+      try {
+        const locationRes = await wx.getLocation()
+        console.log(locationRes)
+      } catch (error) {
+        wx.toast({ title: '您拒绝授权获取位置信息' })
+      }
+    } else {
+      try {
+        const locationRes = await wx.getLocation()
+        console.log(locationRes)
+      } catch (error) {
+        wx.toast({ title: '您拒绝授权获取位置信息' })
+      }
+    }
+  },
+
+  // 对数据进行验证
+  onValidator() {
+    // 定义验证规则
+    const rules = {
+      // key 验证规则的名字，名字需要和验证的数据保持一致
+      name: [
+        { required: true, message: 'name 不能为空' },
+        { type: 'string', message: 'name 不是字符串' },
+        { min: 2, max: 5, message: '名字最少 2 个字，最多是 3 个字' }
+        // pattern 使用正则对数据进行验证
+        // { pattern: '', message: '' }
+
+        // validator 自定义验证规则
+        // { validator: () => {} }
+      ]
+    }
+
+    // 对构造函数进行实例化，同时传入验证规则
+    const validator = new Schema(rules)
+
+    // 需要调用 validate 实例方法，对数据进行验证
+    // 第一个参数：需要验证的数据，要求对象
+    // validate 只会验证和验证规则同名的字段
+    // 第二个参数：回调函数
+    validator.validate(this.data, (errors, fields) => {
+      // 验证成功，errors 是 null
+      // 验证失败，errors 是数组，数组每一项是错误信息
+      // fields 是需要验证的属性，属性值是一个数组，数组中也包含着错误信息
+
+      if (errors) {
+        console.log('验证失败')
+        console.log(errors)
+        console.log(fields)
+      } else {
+        console.log('验证成功')
+        console.log(errors)
+      }
+    })
   }
 })
